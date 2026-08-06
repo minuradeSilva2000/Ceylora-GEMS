a3aa28c1f3a9ace72c4382cd1a515d24b269f4aa
minuradesilva2000 <minuradesilva2000@gmail.com>
2026-08-07 03:14:56 +0530

    test: add Cypress test to verify admin can open order details card

--- cypress/e2e/Admin_jewelllery_order_handling.cy.js
+++ cypress/e2e/Admin_jewelllery_order_handling.cy.js
@@ -180,6 +180,23 @@ describe('Admin order jewellery section - End-to-End Functional Test Suite', () 
      cy.get('.Orders-module-scss-module__z7pe0q__orderList').should('be.visible',{ timeout: 30000 });
      cy.get('input[placeholder="Search by order ref, name, or email…"]').type('ORD-CY-00006')
      cy.get('.Orders-module-scss-module__z7pe0q__orderCard').should('be.visible',{timeout:30000})
-     cy.wait(3000)
+     
   })
+  
+   it('Verify admin can open an order details card successfully',()=>{
+
+     cy.visit('https://houseofceylora.com/admin')
+     cy.get('.AdminLayout-module-scss-module__tIFY2q__mainContent').should('be.visible', { timeout: 30000 });
+     cy.get('.AdminLayout-module-scss-module__tIFY2q__menuBtn ').click()
+     cy.get('aside[class="AdminLayout-module-scss-module__tIFY2q__sidebar AdminLayout-module-scss-module__tIFY2q__open"]').should('be.visible', { timeout: 30000 });
+     cy.get('a[class*="AdminLayout-module-scss-module__tIFY2q__navItem"]').contains('Orders').click()
+     cy.get('.Orders-module-scss-module__z7pe0q__container').should('be.visible', { timeout: 30000 });
+     cy.contains('button','Jewellery').should('be.visible').click()
+     cy.get('.Orders-module-scss-module__z7pe0q__orderList').should('be.visible',{ timeout: 30000 });
+     cy.get('input[placeholder="Search by order ref, name, or email…"]').type('ORD-CY-00002')
+     cy.get('.Orders-module-scss-module__z7pe0q__orderCard').should('be.visible',{timeout:30000})
+     cy.get('.Orders-module-scss-module__z7pe0q__orderCard').first().click()
+     cy.get('.Orders-module-scss-module__z7pe0q__orderCard').should('be.visible', { timeout: 30000 });
+
+   })
 })
