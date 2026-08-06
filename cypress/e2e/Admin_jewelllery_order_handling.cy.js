4db9adbd4fdc20f4e667cd7c7d27cfb23704c4f0
minuradesilva2000 <minuradesilva2000@gmail.com>
2026-08-06 12:18:19 +0530

    test: add Cypress test case to verify gemstone orders filter functionality

--- cypress/e2e/Admin_jewelllery_order_handling.cy.js
+++ cypress/e2e/Admin_jewelllery_order_handling.cy.js
@@ -94,7 +94,7 @@ describe('Admin order jewellery section - End-to-End Functional Test Suite', () 
      cy.get('aside[class="AdminLayout-module-scss-module__tIFY2q__sidebar AdminLayout-module-scss-module__tIFY2q__open"]').should('be.visible', { timeout: 30000 });
      cy.wait(3000)
     })
-      it('Verify admin user can navigate to the Orders page successfully from the sidebar menu',()=>{
+    it('Verify admin user can navigate to the Orders page successfully from the sidebar menu',()=>{
 
      cy.visit('https://houseofceylora.com/admin')
      cy.get('.AdminLayout-module-scss-module__tIFY2q__mainContent').should('be.visible', { timeout: 30000 });
@@ -102,7 +102,18 @@ describe('Admin order jewellery section - End-to-End Functional Test Suite', () 
      cy.get('aside[class="AdminLayout-module-scss-module__tIFY2q__sidebar AdminLayout-module-scss-module__tIFY2q__open"]').should('be.visible', { timeout: 30000 });
      cy.get('a[class*="AdminLayout-module-scss-module__tIFY2q__navItem"]').contains('Orders').click()
      cy.get('.Orders-module-scss-module__z7pe0q__container').should('be.visible', { timeout: 30000 });
-     
+     cy.wait(3000)
+  })
+  it('Verify admin user can filter and view gem orders successfully from the Orders page',()=>{
+
+     cy.visit('https://houseofceylora.com/admin')
+     cy.get('.AdminLayout-module-scss-module__tIFY2q__mainContent').should('be.visible', { timeout: 30000 });
+     cy.get('.AdminLayout-module-scss-module__tIFY2q__menuBtn ').click()
+     cy.get('aside[class="AdminLayout-module-scss-module__tIFY2q__sidebar AdminLayout-module-scss-module__tIFY2q__open"]').should('be.visible', { timeout: 30000 });
+     cy.get('a[class*="AdminLayout-module-scss-module__tIFY2q__navItem"]').contains('Orders').click()
+     cy.get('.Orders-module-scss-module__z7pe0q__container').should('be.visible', { timeout: 30000 });
+     cy.contains('button','Gems').should('be.visible').click()
+     cy.get('.Orders-module-scss-module__z7pe0q__orderList').should('be.visible',{ timeout: 30000 });
   })
 })
 
