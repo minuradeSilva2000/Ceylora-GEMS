07de297994b8c745a8ae1c3596fd5dabb943e1cf
minuradesilva2000 <minuradesilva2000@gmail.com>
2026-08-07 23:56:42 +0530

    test(gems): add Cypress test for admin gem details page navigation

--- cypress/e2e/Admin_gem_inventory_details_entry.cy.js
+++ cypress/e2e/Admin_gem_inventory_details_entry.cy.js
@@ -105,5 +105,18 @@ describe('Admin Gems inventory - End-to-End Functional Test Suite', () => {
      cy.get('a[class*="AdminLayout-module-scss-module__tIFY2q__navItem"]').contains('Gems Inventory').click()
      cy.get('.AdminLayout-module-scss-module__tIFY2q__pageContent').should('be.visible', { timeout: 30000 });
      cy.wait(3000)
-  })
+    })
+    it('Verify admin user can navigate to the Gems Inventory page successfully from the sidebar menu',()=>{
+
+     cy.visit('https://houseofceylora.com/admin')
+     cy.get('.AdminLayout-module-scss-module__tIFY2q__mainContent').should('be.visible', { timeout: 30000 });
+     cy.get('.AdminLayout-module-scss-module__tIFY2q__menuBtn ').click()
+     cy.get('aside[class="AdminLayout-module-scss-module__tIFY2q__sidebar AdminLayout-module-scss-module__tIFY2q__open"]').should('be.visible', { timeout: 30000 });
+     cy.get('a[class*="AdminLayout-module-scss-module__tIFY2q__navItem"]').contains('Gems Inventory').click()
+     cy.get('.AdminLayout-module-scss-module__tIFY2q__pageContent').should('be.visible', { timeout: 30000 });
+     cy.get('a[class="GemInventory-module-scss-module__aAoEsG__viewLink"]').eq(7).click({force:true})
+     cy.url({ timeout: 60000 }).should('include', '/admin/gems/view/14');
+     cy.get('.AdminLayout-module-scss-module__tIFY2q__mainContent').should('be.visible', { timeout: 30000 });
+     cy.get('.GemForm-module-scss-module__6JXq1W__formContainer').should('be.visible', { timeout: 30000 });
+    })
 })
